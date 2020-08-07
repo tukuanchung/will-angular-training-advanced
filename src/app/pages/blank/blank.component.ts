70d7ba7511b9fffab471e7122879277ca309e2d5
tukuanchung <tukuanchung@gmail.com>
2020-08-07 16:05:10 +0800

    熟悉 Router 各種參數用法

--- src/app/pages/blank/blank.component.ts
+++ src/app/pages/blank/blank.component.ts
@@ -7,7 +7,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class BlankComponent implements OnInit {
 
+ name = '';
  type = '';
+ page = 1;
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
@@ -17,6 +19,28 @@ export class BlankComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
         this.type = params.get('type');
     });
+
+    this.name = this.route.snapshot.queryParamMap.get('name');
+
+    this.route.queryParamMap.subscribe(params => {
+      this.name = params.get('name');
+    });
+
+    this.route.queryParamMap.subscribe(params => {
+      this.page = +params.get('page');
+  });
+
+  }
+
+
+  goPage(pageNo: number) {
+    this.router.navigateByUrl('/pages/blank?page=' + pageNo);
+
+    // this.router.navigate(['/pages/blank'], {
+    //   queryParams: {
+    //     page: pageNo
+    //   }
+    // });
   }
 
 }
