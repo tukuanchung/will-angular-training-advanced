e0b977954dd59a1564b0bec1402ecfe4d2c71c11
tukuanchung <tukuanchung@gmail.com>
2020-08-14 15:23:45 +0800

    示範 Reactive Forms 表單驗證寫法

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login2.component.html',
@@ -11,18 +11,36 @@ export class Login2Component implements OnInit, OnDestroy {
 
   constructor(private fb: FormBuilder) { }
 
-
   ngOnInit(): void {
     document.body.className = 'bg-gradient-primary';
 
-    this.form = this.fb.group({
-      email: 'tukuanchung@mgmail.com',
-      password: '123123',
+    // this.form = this.fb.group({
+    //   email: 'tukuanchung@mgmail.com',
+    //   password: '123123',
+    //   rememberMe: true
+    // });
+
+    // this.form = this.fb.group({
+    //   email: ['tukuanchung@gmail.com', [Validators.required, Validators.email]],
+    //   password: ['123123', [ Validators.required, Validators.minLength(3)]],
+    //   rememberMe: true
+    // });
+
+    this.form = this.fb.group( {
+      email: this.fb.control('tukuanchung@gmail.com', {
+          validators : [ Validators.required, Validators.email],
+          updateOn: 'blur'
+      }),
+      password: this.fb.control('123123', {
+        validators: [ Validators.required, Validators.minLength(3)]
+      }),
       rememberMe: true
+
     });
   }
 
   ngOnDestroy(): void {
     document.body.className = '';
   }
+
 }
